6ac036e74dd5cc00e7435eb5d9e992c063423e53
Reini Urban <rurban@cpanel.net>
2015-10-07 18:00:12 +0200

    C 5.22: WIP padname_with_str
    Perl_newPADNAMEouter crashes in PadnameREFCNT(PADNAME_FROM_PV(PadnamePV(outer)))++
    when accessing a padname_with_str, which is an extended PADNAME, depending on
    2 defines (inlined or ptr). XXX Not sure if can support static PADNAME at all with this scheme.
    
    (cherry picked from commit d0cf4d9ae50268622352d724e3423d5e82ab4908)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>
    
    C 5.22: fix padname_with_str, broke test 10
    
    need a special memory layout for 5.22 padnames.
    Note: detected issue GH #230.
    Added a new limitation: lexical variable name max 60 chars long. GH #229
    
    (cherry picked from commit ff347777799963108431c6b5ca3a457085190ae3)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>
    
    C 5.22: fix GH #230
    
    (cherry picked from commit bf36416ca7de4d05512bb854ed12af24a4191753)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>
    
    Merge these three commits together.

--- lib/B/C/OverLoad/B/PADNAME.pm
+++ lib/B/C/OverLoad/B/PADNAME.pm
@@ -32,25 +32,40 @@ sub save {
 
     my $refcnt = $pn->REFCNT;
     $refcnt++ if $refcnt < 1000;    # XXX protect from free, but allow SvREFCOUNT_IMMORTAL
-    padnamesect()->comment(" pv, ourstash, type, low, high, refcnt, gen, len, flags");
+    padnamesect()->comment(" pv, ourstash, type, low, high, refcnt, gen, len, flags, str");
+
+    my $str  = $pn->PVX;
+    my $cstr = cstring($str);
     padnamesect()->add(
         sprintf(
-            "%s, %s, {%s}, %u, %u, %s, %i, %u, 0x%x",
-            cstring( $pn->PVX ),
+            # ignore warning: initializer-string for array of chars is too long
+            "%s, %s, {%s}, %u, %u, %s, %i, %u, 0x%x, %s",
+            'NULL',
             is_constant($sn) ? "(HV*)$sn"              : 'Nullhv',
             is_constant($tn) ? "(HV*)$tn"              : 'Nullhv',
             $is_fake         ? $pn->COP_SEQ_RANGE_LOW  : 0,
             $is_fake         ? $pn->COP_SEQ_RANGE_HIGH : 0,
             $refcnt >= 1000 ? sprintf( "0x%x", $refcnt ) : "$refcnt /* +1 */",
-            $gen, $pn->LEN, $flags
+            $gen, $pn->LEN, $flags, $cstr
         )
     );
+
+    if ( $pn->LEN > 60 ) {
+
+        # Houston we have a problem, need to allocate this padname dynamically. Not done yet
+        die "Internal Error: Overlong name of lexical variable $cstr for $fullname [#229]";
+    }
+
     my $s = "&padname_list[" . padnamesect()->index . "]";
 
-    padnamesect()->debug( $fullname, $pn->flagspv ) if debug('flags');
+    padnamesect()->debug( $fullname . " " . $str, $pn->flagspv ) if debug('flags');
 
     init()->add("SvOURSTASH_set($s, $sn);")     unless $sn eq 'Nullsv';
     init()->add("PadnameTYPE($s) = (HV*) $tn;") unless $tn eq 'Nullsv';
+
+    # 5.22 needs the buffer to be at the end, and the pointer pointing to it.
+    # We allocate a static buffer and adjust pv at init.
+    init()->add("PadnamePV($s) = ((MyPADNAME *)$s)->xpadn_str;");
     push @B::C::static_free, $s;
 
     return savesym( $pn, $s );
