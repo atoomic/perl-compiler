f03e29954f9b53881fcb3f1842ebb5f0ef9048ff
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-11-03 10:23:52 -0600

    wip

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -557,60 +557,6 @@ sub padop_name {
     }
 }
 
-sub svop_name {
-    my $op = shift;
-    my $cv = shift;
-    my $sv;
-    if ( $op->can('name') and $op->name eq 'padsv' ) {
-        return padop_name( $op, $cv );
-    }
-    else {
-        if ( !$op->can("sv") ) {
-            if ( ref($op) eq 'B::PMOP' and $op->pmreplroot->can("sv") ) {
-                $sv = $op->pmreplroot->sv;
-            }
-            else {
-                $sv = $op->first->sv
-                  unless $op->flags & 4
-                  or ( $op->name eq 'const' and $op->flags & 34 )
-                  or $op->first->can("sv");
-            }
-        }
-        else {
-            $sv = $op->sv;
-        }
-        if ( $sv and $$sv ) {
-            if ( $sv->FLAGS & SVf_ROK ) {
-                return '' if $sv->isa("B::NULL");
-                my $rv = $sv->RV;
-                if ( $rv->isa("B::PVGV") ) {
-                    my $o = $rv->IO;
-                    return $o->STASH->NAME if $$o;
-                }
-                return '' if $rv->isa("B::PVMG");
-                return $rv->STASH->NAME;
-            }
-            else {
-                if ( $op->name eq 'gvsv' or $op->name eq 'gv' ) {
-                    return $sv->STASH->NAME . '::' . $sv->NAME;
-                }
-
-                return
-                    $sv->can('STASH') ? $sv->STASH->NAME
-                  : $sv->can('NAME')  ? $sv->NAME
-                  :                     $sv->PV;
-            }
-        }
-    }
-}
-
-# return the next COP for file and line info
-sub nextcop {
-    my $op = shift;
-    while ( $op and ref($op) ne 'B::COP' and ref($op) ne 'B::NULL' ) { $op = $op->next; }
-    return ( $op and ref($op) eq 'B::COP' ) ? $op : undef;
-}
-
 # Maybe move to the unused optimizer
 sub mark_skip {
     for (@_) {
--- lib/B/C_heavy.pl
+++ lib/B/C_heavy.pl
@@ -6,6 +6,11 @@ BEGIN {
     *Config = \%B::C::Flags::Config;
 }
 
+use B qw(minus_c sv_undef walkoptree walkoptree_slow main_root main_start peekop
+  class cchar svref_2object compile_stats comppadlist hash
+  init_av end_av opnumber cstring
+  HEf_SVKEY SVf_POK SVf_ROK SVf_IOK SVf_NOK SVf_IVisUV SVf_READONLY);
+
 use B::C::File qw( init2 init0 init decl free
   heksect binopsect condopsect copsect padopsect listopsect logopsect
   opsect pmopsect pvopsect svopsect unopsect svsect xpvsect xpvavsect xpvhvsect xpvcvsect xpvivsect xpvuvsect
@@ -13,5 +18,60 @@ use B::C::File qw( init2 init0 init decl free
 );
 
 use B::C::Packages qw/is_package_used mark_package_unused mark_package_used mark_package_removed get_all_packages_used/;
+
+sub svop_name {
+    my $op = shift;
+    my $cv = shift;
+    my $sv;
+    if ( $op->can('name') and $op->name eq 'padsv' ) {
+        return padop_name( $op, $cv );
+    }
+    else {
+        if ( !$op->can("sv") ) {
+            if ( ref($op) eq 'B::PMOP' and $op->pmreplroot->can("sv") ) {
+                $sv = $op->pmreplroot->sv;
+            }
+            else {
+                $sv = $op->first->sv
+                  unless $op->flags & 4
+                  or ( $op->name eq 'const' and $op->flags & 34 )
+                  or $op->first->can("sv");
+            }
+        }
+        else {
+            $sv = $op->sv;
+        }
+        if ( $sv and $$sv ) {
+            if ( $sv->FLAGS & SVf_ROK ) {
+                return '' if $sv->isa("B::NULL");
+                my $rv = $sv->RV;
+                if ( $rv->isa("B::PVGV") ) {
+                    my $o = $rv->IO;
+                    return $o->STASH->NAME if $$o;
+                }
+                return '' if $rv->isa("B::PVMG");
+                return $rv->STASH->NAME;
+            }
+            else {
+                if ( $op->name eq 'gvsv' or $op->name eq 'gv' ) {
+                    return $sv->STASH->NAME . '::' . $sv->NAME;
+                }
+
+                return
+                    $sv->can('STASH') ? $sv->STASH->NAME
+                  : $sv->can('NAME')  ? $sv->NAME
+                  :                     $sv->PV;
+            }
+        }
+    }
+}
+
+# return the next COP for file and line info
+sub nextcop {
+    my $op = shift;
+    while ( $op and ref($op) ne 'B::COP' and ref($op) ne 'B::NULL' ) { $op = $op->next; }
+    return ( $op and ref($op) eq 'B::COP' ) ? $op : undef;
+}
+
 
 1;
