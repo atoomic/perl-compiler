30cca75d0b6116eb4be10e71046f801b60fa89aa
Todd Rinaldo <toddr@cpan.org>
2017-05-24 18:36:42 -0500

    Simplify CV save

--- lib/B/C/OverLoad/B/CV.pm
+++ lib/B/C/OverLoad/B/CV.pm
@@ -4,12 +4,12 @@ use strict;
 
 use B::C::Flags ();
 
-use B qw/cstring svref_2object CVf_ANON CVf_ANONCONST CVf_CONST main_cv SVf_ROK SVp_POK SVf_IOK SVf_UTF8 SVs_PADSTALE CVf_WEAKOUTSIDE/;
+use B qw/cstring svref_2object CVf_ANON CVf_ANONCONST CVf_CONST CVf_NAMED main_cv SVf_ROK SVp_POK SVf_IOK SVf_UTF8 SVs_PADSTALE CVf_WEAKOUTSIDE/;
 use B::C::Config;
 use B::C::Decimal qw/get_integer_value/;
-use B::C::Save qw/savepvn constpv/;
+use B::C::Save qw/savepvn constpv savecowpv/;
 use B::C::Save::Hek qw/save_shared_he/;
-use B::C::File qw/init init2 decl svsect xpvcvsect symsect/;
+use B::C::File qw/init init2 decl svsect xpvcvsect symsect init_static_assignments/;
 use B::C::Helpers qw/get_cv_string strlen_flags set_curcv/;
 use B::C::Helpers::Symtable qw/objsym savesym delsym/;
 use B::C::Optimizer::ForceHeavy qw/force_heavy/;
@@ -18,6 +18,208 @@ my (%cvforward);
 my $cv_index      = 0;
 my $initsub_index = 0;
 my $anonsub_index = 0;
+
+sub do_save {
+    my ( $cv, $origname ) = @_;
+    debug( cv => "CV ==  %s", $origname );
+
+    my $sv_ix = svsect()->add('FAKE_GV');
+    my $sym = savesym( $cv, "&sv_list[$sv_ix]" );
+
+    my $xmg_stash = typecast_stash_save( $cv->SvSTASH->save );
+    my $cv_stash  = typecast_stash_save( $cv->STASH->save );
+
+    my $pv = $cv->PV;
+
+    my ( $savesym, $cur, $len );
+    if ( $cv->CUR ) {
+        ( $savesym, $cur, $len ) = savecowpv($pv);
+    }
+
+    # need to survive cv_undef as there is no protection against static CVs
+    my $refcnt = $cv->REFCNT + 1;
+
+    my $root = $cv->ROOT;
+
+    my $startfield = $cv->save_optree();
+
+    my ( undef, undef, $xcv_file ) = savecowpv( $cv->FILE || '' );
+
+    xpvcvsect->comment("xmg_stash, xmg_u, xpv_cur, xpv_len_u, xcv_stash, xcv_start_u, xcv_root_u, xcv_gv_u, xcv_file, xcv_padlist_u, xcv_outside, xcv_outside_seq, xcv_flags, xcv_depth");
+    my $xpvcv_ix = xpvcvsect->sadd(
+        "%s, %s, %u, {%u}, %s, {%s}, {s\\_%x}, %s, (char*) %s, {%s}, (CV*)%s, %s, 0x%x, %d",
+        $xmg_stash,                               # xmg_stash
+        $cv->save_magic($origname),               # xmg_u
+        $cur,                                     # xpv_cur
+        $len,                                     # xpv_len_u
+        $cv_stash,                                # xcv_stash
+        $startfield,                              # xcv_start_u
+        $$root,                                   # xcv_root_u
+        $cv->get_xcv_gv_u,                        # $xcv_gv_u, # xcv_gv_u
+        $xcv_file,                                # xcv_file
+        $cv->cv_save_padlist($origname),          # xcv_padlist_u
+        $cv->get_cv_outside(),                    # xcv_outside
+        get_integer_value( $cv->OUTSIDE_SEQ ),    # xcv_outside_seq
+        $cv->CvFLAGS,                             # xcv_flags
+        $cv->DEPTH                                # xcv_depth
+    );
+
+    # svsect()->comment("any, refcnt, flags, sv_u");
+    my $sv_ix = svsect->supdate( $sv_ix, "(XPVCV*)&xpvcv_list[%u], %Lu, 0x%x, {0}", $xpvcv_ix, $cv->REFCNT + 1, $cv->FLAGS, $savesym );
+
+    return $sym;
+}
+
+sub typecast_stash_save {
+    my $symbol = shift or return;
+    $symbol = q{Nullhv}       if $symbol eq 'Nullsv';
+    $symbol = "(HV*) $symbol" if $symbol ne 'Nullhv';
+
+    return $symbol;
+}
+
+sub get_cv_outside {
+    my ($cv) = @_;
+    my $xcv_outside = ${ $cv->OUTSIDE };
+    if ( $xcv_outside == ${ main_cv() } ) {
+
+        # Provide a temp. debugging hack for CvOUTSIDE. The address of the symbol &PL_main_cv
+        # is known to the linker, the address of the value PL_main_cv not. This is set later
+        # (below) at run-time.
+        $xcv_outside = '&PL_main_cv';
+    }
+    elsif ( ref( $cv->OUTSIDE ) eq 'B::CV' ) {
+        $xcv_outside = 0;    # just a placeholder for a run-time GV
+    }
+    elsif ($xcv_outside) {
+        $cv->OUTSIDE->save;
+    }
+
+    return $xcv_outside;
+}
+
+sub cv_save_padlist {
+    my ( $cv, $origname ) = @_;
+
+    my $padlist = $cv->PADLIST;
+
+    $$padlist or return 'NULL';
+    my $fullname = $cv->get_full_name($origname);
+
+    return $padlist->save( $fullname . ' :pad', $cv );
+}
+
+sub get_full_name {
+    my ( $cv, $origname ) = @_;
+
+    my $fullname = '';
+
+    my $gv     = $cv->GV;
+    my $cvname = '';
+    if ( $gv and $$gv ) {
+        $cvname = $gv->NAME;
+        my $cvstashname = $gv->STASH->NAME;
+        $fullname = $cvstashname . '::' . $cvname;
+
+        # XXX gv->EGV does not really help here
+        if ( $cvname eq '__ANON__' ) {
+            if ($origname) {
+                $cvname = $fullname = $origname;
+                $cvname =~ s/^\Q$cvstashname\E::(.*)( :pad\[.*)?$/$1/ if $cvstashname;
+                $cvname =~ s/^.*:://;
+                if ( $cvname =~ m/ :pad\[.*$/ ) {
+                    $cvname =~ s/ :pad\[.*$//;
+                    $cvname = '__ANON__' if is_phase_name($cvname);
+                    $fullname = $cvstashname . '::' . $cvname;
+                }
+            }
+            else {
+                $cvname   = $gv->EGV->NAME;
+                $fullname = $cvstashname . '::' . $cvname;
+            }
+        }
+
+    }
+    elsif ( $cv->is_lexsub($gv) ) {
+        $fullname = $cv->NAME_HEK;
+        $fullname = '' unless defined $fullname;
+    }
+
+    my $isconst = $cv->CvFLAGS & CVf_CONST;
+    if ( !$isconst && $cv->XSUB && ( $cvname ne "INIT" ) ) {
+        my $egv       = $gv->EGV;
+        my $stashname = $egv->STASH->NAME;
+        $fullname = $stashname . '::' . $cvname;
+    }
+
+    return $fullname;
+
+}
+
+sub get_xcv_gv_u {
+    my ($cv) = @_;
+
+    if ( $cv->CvFLAGS & CVf_NAMED ) {    #HEK (.xcv_hek)
+        my $pv = $cv->PV || "unknownERROR";
+        my $xcv_gv_u = defined $pv ? sprintf( "{.xcv_hek=(HEK*) ((void*) %s + sizeof(HE)) }", save_shared_he($pv) ) : "{0}";    # xcv_gv_u
+        $xcv_gv_u =~ s/sharedhe_list\[(\d+)\]/&sHe$1/;
+        return $xcv_gv_u;
+    }
+
+    #GV (.xcv_gv)
+    my $xcv_gv_u = $cv->GV ? $cv->GV->save : 'Nullsv';
+
+    return $xcv_gv_u if $xcv_gv_u eq 'Nullsv';
+
+    return sprintf( "{ .xcv_gv = %s }", $xcv_gv_u );
+}
+
+sub save_optree {
+    my ($cv) = @_;
+
+    my $root = $cv->ROOT;
+
+    return 0 unless ($$root);
+
+    my $gv = $cv->GV;
+
+    my $ppname;
+    my $fullname;
+
+    if ( $cv->is_lexsub($gv) ) {
+        my $name = $cv->can('NAME_HEK') ? $cv->NAME_HEK : "anonlex";
+        $ppname   = "pp_lexsub_" . $name;
+        $fullname = "<lex>" . $name;
+    }
+    elsif ( $gv and $$gv ) {
+        my ( $stashname, $gvname );
+        $stashname = $gv->STASH->NAME;
+        $gvname    = $gv->NAME;
+        $fullname  = $stashname . '::' . $gvname;
+        $ppname    = ( ${ $gv->FORM } == $$cv ) ? "pp_form_" : "pp_sub_";
+        if ( $gvname ne "__ANON__" ) {
+            $ppname .= ( $stashname eq "main" ) ? $gvname : "$stashname\::$gvname";
+            $ppname =~ s/::/__/g;
+            $ppname =~ s/(\W)/sprintf("0x%x", ord($1))/ge;
+            if ( $gvname eq "INIT" ) {
+                $ppname .= '_' . $initsub_index;
+                $initsub_index++;
+            }
+        }
+    }
+    if ( !$ppname ) {
+        $ppname = "pp_anonsub_$anonsub_index";
+        $anonsub_index++;
+    }
+
+    my $startfield = B::C::saveoptree( $ppname, $root, $cv->START, $cv->PADLIST->ARRAY );    # XXX padlist is ignored
+
+    # XXX missing cv_start for AUTOLOAD on 5.8
+    $startfield = objsym( $root->next ) unless $startfield;                                  # 5.8 autoload has only root
+    $startfield = "0" unless $startfield;                                                    # XXX either CONST ANON or empty body
+
+    return $startfield;
+}
 
 sub is_lexsub {
     my ( $cv, $gv ) = @_;
@@ -32,7 +234,7 @@ sub is_phase_name {
 
 sub Dummy_initxs { }
 
-sub do_save {
+sub do_save_old {
     my ( $cv, $origname ) = @_;
     my $sym;
     my $gv = $cv->GV;
@@ -517,10 +719,10 @@ sub do_save {
         "Nullhv, %s, %u, {%u}, %s, {%s}, {s\\_%x}, {%s}, %s, {%s}, (CV*)%s, %s, 0x%x, %d",
         $cv->save_magic($fullname), $cur, $len, "Nullhv",    #CvSTASH later
         $startfield, $$root,
-        "0",                     #GV later
-        "NULL",                  #cvfile later (now a HEK)
+        "0",                                                 #GV later
+        "NULL",                                              #cvfile later (now a HEK)
         $padlistsym,
-        $xcv_outside,            #if main_cv set later
+        $xcv_outside,                                        #if main_cv set later
         get_integer_value( $cv->OUTSIDE_SEQ ),
         $CvFLAGS,
         $cv->DEPTH
