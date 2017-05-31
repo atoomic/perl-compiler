18a60dafa97f72158401a8092d68de942c7a7dc7
Todd Rinaldo <toddr@cpan.org>
2017-05-30 19:41:21 -0500

    Revert "What happens when we save STDIO GVs?"
    This reverts commit f0d23d95562aa90cc7463d8b6cc4e2b2bb789af8.

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -173,7 +173,7 @@ sub cleanup_stashes {
     #}
 
     # cleanup special variables
-    foreach my $k ( qw{ BEGIN ARGV ENV + - }, 1 .. 9 ) {
+    foreach my $k ( qw{ BEGIN ARGV ENV STDERR STDIN STDOUT stderr stdin stdout + - }, 1 .. 9 ) {
         delete $stashes->{$k};
     }
 
