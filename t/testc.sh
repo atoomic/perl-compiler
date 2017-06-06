0e3cd6beb5da3e7eea2feb2444a97105819ba83f
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-06-06 15:07:17 -0600

    Remove xtestc/0174.pl
    \C no longer supported in regex

--- t/testc.sh
+++ t/testc.sh
@@ -688,18 +688,6 @@ Constant subroutine BEGIN redefined at ./ccode173.pl line 2.
 ok 1
 ok 2
 ok 3'
-tests[174]='
-my $str = "\x{10000}\x{800}";
-no warnings "utf8";
-{ use bytes; $str =~ s/\C\C\z//; }
-my $ref = "\x{10000}\0";
-print "ok 1\n" if ~~$str eq $ref;
-$str = "\x{10000}\x{800}";
-{ use bytes; $str =~ s/\C\C\z/\0\0\0/; }
-my $ref = "\x{10000}\0\0\0\0";
-print "ok 2\n" if ~~$str eq $ref;'
-result[174]='ok 1
-ok 2'
 tests[175]='{
   # note that moving the use in an eval block solve the problem
   use warnings NONFATAL => all;
