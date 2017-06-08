6dc6f62795c0d89a2c0fc8c63fce4c0dadb053d8
Todd Rinaldo <toddr@cpan.org>
2017-06-07 19:12:10 -0500

    Re-attach REGEX magic when using a new SV to runtime build the ANY

--- lib/B/C/OverLoad/B/REGEXP.pm
+++ lib/B/C/OverLoad/B/REGEXP.pm
@@ -18,35 +18,48 @@ sub do_save {
     $cur -= length( $sv->PV ) - length($pv);
     my $cstr = cstring($pv);
 
+    my $magic_stash = $sv->save_magic_stash;
+    my $magic       = $sv->save_magic($fullname);
+
     # Unfortunately this XPV is needed temp. Later replaced by struct regexp.
     # STATIC HV: Static stash please.
     my $xpv_ix = xpvsect()->saddl(
-        "%s"   => $sv->save_magic_stash,         # xmg_stash
-        "{%s}" => $sv->save_magic($fullname),    # xmg_u
-        "%u"   => $cur,                          # xpv_cur
-        "{%u}" => 0                              # xpv_len_u STATIC_HV: length is 0 ???
+        "%s"   => $magic_stash,    # xmg_stash
+        "{%s}" => $magic,          # xmg_u
+        "%u"   => $cur,            # xpv_cur
+        "{%u}" => 0                # xpv_len_u STATIC_HV: length is 0 ???
     );
-    my $ix = svsect()->sadd(
-        "&xpv_list[%d], %Lu, 0x%x, {NULL}",
-        $xpv_ix, $sv->REFCNT, $sv->FLAGS
-    );
+    my $xpv_sym = sprintf( "&xpv_list[%d]", $xpv_ix );
+
+    my $ix = svsect()->sadd( "%s, %Lu, 0x%x, {NULL}", $xpv_sym, $sv->REFCNT + 1, $sv->FLAGS );
+    my $sym = sprintf( "&sv_list[%d]", $ix );
     debug( rx => "Saving RX $cstr to sv_list[$ix]" );
 
-    if ( $sv->EXTFLAGS & RXf_EVAL_SEEN ) {
-        init()->add("PL_hints |= HINT_RE_EVAL;");
-    }
-
     # replace sv_any->XPV with struct regexp. need pv and extflags
-    init()->sadd( 'SvANY(&sv_list[%d]) = SvANY(CALLREGCOMP(newSVpvn(%s, %d), 0x%x));', $ix, $cstr, $cur, $sv->EXTFLAGS );
-    if ( $sv->EXTFLAGS & RXf_EVAL_SEEN ) {
-        init()->add("PL_hints &= ~HINT_RE_EVAL;");
-    }
+    init()->no_split;
+    init()->add('{');
+    init()->indent(+1);
 
-    init()->add("sv_list[$ix].sv_u.svu_rx = (struct regexp*)sv_list[$ix].sv_any;");
+    # Re-compile into an SV.
+    init()->add("PL_hints |= HINT_RE_EVAL;") if ( $sv->EXTFLAGS & RXf_EVAL_SEEN );
+    init()->sadd( 'REGEXP* regex_sv = CALLREGCOMP(newSVpvn(%s, %d), 0x%x);', $cstr, $cur, $sv->EXTFLAGS );
+    init()->add("PL_hints &= ~HINT_RE_EVAL;") if ( $sv->EXTFLAGS & RXf_EVAL_SEEN );
+
+    init()->sadd( 'SvANY(%s) = SvANY(regex_sv);', $sym );
+
+    my $without_amp = $sym;
+    $without_amp =~ s/^&//;
+    init()->sadd( "%s.sv_u.svu_rx = (struct regexp*)SvANY(regex_sv);", $without_amp );
+    init()->sadd( "ReANY(%s)->xmg_stash =  %s;",                       $sym, $magic_stash );
+    init()->sadd( "ReANY(%s)->xmg_u.xmg_magic =  %s;",                 $sym, $magic );
+
+    init()->indent(-1);
+    init()->add('}');
+    init()->split;
 
     svsect()->debug( $fullname, $sv );
 
-    return sprintf( "&sv_list[%d]", $ix );
+    return $sym;
 }
 
 1;
