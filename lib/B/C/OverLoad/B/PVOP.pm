22b40c30959624d90a142885d04054848f49dd43
Todd Rinaldo <toddr@cpan.org>
2017-06-12 13:36:07 -0500

    Can PVOP be a static COW?

--- lib/B/C/OverLoad/B/PVOP.pm
+++ lib/B/C/OverLoad/B/PVOP.pm
@@ -2,23 +2,18 @@ package B::PVOP;
 
 use strict;
 
-use B qw/cstring/;
-
-use B::C::File qw/loopsect pvopsect init/;
-use B::C::Helpers qw/strlen_flags/;
+use B::C::File qw/pvopsect/;
+use B::C::Save qw/savecowpv/;
 
 sub do_save {
     my ( $op, $level ) = @_;
 
-    # op_pv must be dynamic
-    loopsect()->comment_common("pv");
+    my ( $cow_sym, $cur, $len ) = savecowpv( $op->pv );
 
-    my $ix = pvopsect()->sadd( "%s, NULL", $op->_save_common );
+    pvopsect()->comment_common("pv");
+
+    my $ix = pvopsect()->sadd( "%s, %s", $op->_save_common, $cow_sym );
     pvopsect()->debug( $op->name, $op );
-
-    my ( $cstring, $cur, $utf8 ) = strlen_flags( $op->pv );    # utf8 in op_private as OPpPV_IS_UTF8 (0x80)
-
-    init()->sadd( "pvop_list[%d].op_pv = savesharedpvn(%s, %u);", $ix, $cstring, $cur );
 
     return "(OP*)&pvop_list[$ix]";
 }
