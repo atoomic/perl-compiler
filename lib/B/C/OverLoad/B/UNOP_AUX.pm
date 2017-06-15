8b93b72b15069c49dfde5bc7bd9a39f78e05fc13
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-06-15 15:05:43 -0600

    UNOP_AUX - do static link when possible

--- lib/B/C/OverLoad/B/UNOP_AUX.pm
+++ lib/B/C/OverLoad/B/UNOP_AUX.pm
@@ -35,54 +35,8 @@ sub do_save {
         unless ( ref $item ) {
 
             # symbolize MDEREF action
-            my $cmt = 'action';
-            if ( verbose() ) {
-                if ( $op->name eq 'multideref' ) {
-                    my $act = $item & 0xf;    # MDEREF_ACTION_MASK
-                    $cmt = 'AV_pop_rv2av_aelem'          if $act == 1;
-                    $cmt = 'AV_gvsv_vivify_rv2av_aelem'  if $act == 2;
-                    $cmt = 'AV_padsv_vivify_rv2av_aelem' if $act == 3;
-                    $cmt = 'AV_vivify_rv2av_aelem'       if $act == 4;
-                    $cmt = 'AV_padav_aelem'              if $act == 5;
-                    $cmt = 'AV_gvav_aelem'               if $act == 6;
-                    $cmt = 'HV_pop_rv2hv_helem'          if $act == 8;
-                    $cmt = 'HV_gvsv_vivify_rv2hv_helem'  if $act == 9;
-                    $cmt = 'HV_padsv_vivify_rv2hv_helem' if $act == 10;
-                    $cmt = 'HV_vivify_rv2hv_helem'       if $act == 11;
-                    $cmt = 'HV_padhv_helem'              if $act == 12;
-                    $cmt = 'HV_gvhv_helem'               if $act == 13;
-                    my $idx = $item & 0x30;    # MDEREF_INDEX_MASK
-                    $cmt .= ''             if $idx == 0x0;
-                    $cmt .= ' INDEX_const' if $idx == 0x10;
-                    $cmt .= ' INDEX_padsv' if $idx == 0x20;
-                    $cmt .= ' INDEX_gvsv'  if $idx == 0x30;
-                }
-                elsif ( $op->name eq 'signature' ) {    # cperl only for now
-                    my $act = $item & 0xf;              # SIGNATURE_ACTION_MASK
-                    $cmt = 'reload'            if $act == 0;
-                    $cmt = 'end'               if $act == 1;
-                    $cmt = 'padintro'          if $act == 2;
-                    $cmt = 'arg'               if $act == 3;
-                    $cmt = 'arg_default_none'  if $act == 4;
-                    $cmt = 'arg_default_undef' if $act == 5;
-                    $cmt = 'arg_default_0'     if $act == 6;
-                    $cmt = 'arg_default_1'     if $act == 7;
-                    $cmt = 'arg_default_iv'    if $act == 8;
-                    $cmt = 'arg_default_const' if $act == 9;
-                    $cmt = 'arg_default_padsv' if $act == 10;
-                    $cmt = 'arg_default_gvsv'  if $act == 11;
-                    $cmt = 'arg_default_op'    if $act == 12;
-                    $cmt = 'array'             if $act == 13;
-                    $cmt = 'hash'              if $act == 14;
-                    my $idx = $item & 0x3F;    # SIGNATURE_MASK
-                    $cmt .= ''           if $idx == 0x0;
-                    $cmt .= ' flag skip' if $idx == 0x10;
-                    $cmt .= ' flag ref'  if $idx == 0x20;
-                }
-                else {
-                    die "Unknown UNOP_AUX op {$op->name}";
-                }
-            }
+            my $cmt = $op->get_action_name($item);
+
             $action = $item;
             debug( hv => $op->name . " action $action $cmt" );
             $s .= sprintf( "\t,{.uv=0x%x} \t/* %s: %u */\n", $item, $cmt, $item );
@@ -112,9 +66,14 @@ sub do_save {
                 }
             }
             else {
-                # gv or other late inits
-                $s .= "\t,{.sv=Nullsv} \t/* $itemsym */\n";
-                init()->add("unopaux_item${ix}[$i].sv = (SV*)$itemsym;");
+                if ( $itemsym =~ qr{^PL_} ) {
+                    $s .= "\t,{.sv=Nullsv} \t/* $itemsym */\n";
+                    init()->add("unopaux_item${ix}[$i].sv = (SV*)$itemsym;");
+                }
+                else {
+                    ## gv or other late inits
+                    $s .= "\t,{.sv = (SV*) $itemsym}\n";
+                }
             }
         }
         $i++;
@@ -127,6 +86,60 @@ sub do_save {
     do_labels( $op, $level + 1, 'first' );
 
     return $sym;
+}
+
+sub get_action_name {
+    my ( $op, $item ) = @_;
+
+    my $cmt = 'action';
+    if ( $op->name eq 'multideref' ) {
+        my $act = $item & 0xf;    # MDEREF_ACTION_MASK
+        $cmt = 'AV_pop_rv2av_aelem'          if $act == 1;
+        $cmt = 'AV_gvsv_vivify_rv2av_aelem'  if $act == 2;
+        $cmt = 'AV_padsv_vivify_rv2av_aelem' if $act == 3;
+        $cmt = 'AV_vivify_rv2av_aelem'       if $act == 4;
+        $cmt = 'AV_padav_aelem'              if $act == 5;
+        $cmt = 'AV_gvav_aelem'               if $act == 6;
+        $cmt = 'HV_pop_rv2hv_helem'          if $act == 8;
+        $cmt = 'HV_gvsv_vivify_rv2hv_helem'  if $act == 9;
+        $cmt = 'HV_padsv_vivify_rv2hv_helem' if $act == 10;
+        $cmt = 'HV_vivify_rv2hv_helem'       if $act == 11;
+        $cmt = 'HV_padhv_helem'              if $act == 12;
+        $cmt = 'HV_gvhv_helem'               if $act == 13;
+        my $idx = $item & 0x30;    # MDEREF_INDEX_MASK
+        $cmt .= ''             if $idx == 0x0;
+        $cmt .= ' INDEX_const' if $idx == 0x10;
+        $cmt .= ' INDEX_padsv' if $idx == 0x20;
+        $cmt .= ' INDEX_gvsv'  if $idx == 0x30;
+    }
+    elsif ( $op->name eq 'signature' ) {    # cperl only for now
+        my $act = $item & 0xf;              # SIGNATURE_ACTION_MASK
+        $cmt = 'reload'            if $act == 0;
+        $cmt = 'end'               if $act == 1;
+        $cmt = 'padintro'          if $act == 2;
+        $cmt = 'arg'               if $act == 3;
+        $cmt = 'arg_default_none'  if $act == 4;
+        $cmt = 'arg_default_undef' if $act == 5;
+        $cmt = 'arg_default_0'     if $act == 6;
+        $cmt = 'arg_default_1'     if $act == 7;
+        $cmt = 'arg_default_iv'    if $act == 8;
+        $cmt = 'arg_default_const' if $act == 9;
+        $cmt = 'arg_default_padsv' if $act == 10;
+        $cmt = 'arg_default_gvsv'  if $act == 11;
+        $cmt = 'arg_default_op'    if $act == 12;
+        $cmt = 'array'             if $act == 13;
+        $cmt = 'hash'              if $act == 14;
+        my $idx = $item & 0x3F;    # SIGNATURE_MASK
+        $cmt .= ''           if $idx == 0x0;
+        $cmt .= ' flag skip' if $idx == 0x10;
+        $cmt .= ' flag ref'  if $idx == 0x20;
+    }
+    else {
+        die "Unknown UNOP_AUX op {$op->name}";
+    }
+
+    return $cmt;
+
 }
 
 1;
