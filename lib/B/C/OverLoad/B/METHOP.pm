b8aa00ed0ccd0a8922397ad78eff6c22f52a3791
Todd Rinaldo <toddr@cpan.org>
2017-06-29 15:41:21 -0500

    Save METHOP sym and methopsect early so recursive calls don't use the wrong methop_list index

--- lib/B/C/OverLoad/B/METHOP.pm
+++ lib/B/C/OverLoad/B/METHOP.pm
@@ -19,21 +19,23 @@ sub do_save {
 
     my $union = $name eq 'method' ? "{.op_first=(OP*)%s}" : "{.op_meth_sv=(SV*)%s}";
 
-    my $ix     = methopsect()->index + 1;
+    my $ix = methopsect()->add('FAKE_METHOP');
+    my $sym = savesym( $op, "(OP*)&methop_list[$ix]" );    # save it before do_labels
+
     my $rclass = $op->rclass->save("op_rclass_sv");
     if ( $rclass && $rclass =~ /^&sv_list/ ) {
         my $rclass_name = $op->rclass()->PV();
-        my $sym         = savestashpv($rclass_name);
-        if ( $sym && $sym =~ /^&sv_list/ ) {
-            init2()->sadd( "Perl_mro_method_changed_in((HV*) %s);  /* %s */", $sym, $rclass_name );
+        my $stash_sym   = savestashpv($rclass_name);
+        if ( $stash_sym && $stash_sym =~ /^&sv_list/ ) {
+            init2()->sadd( "Perl_mro_method_changed_in((HV*) %s);  /* %s */", $stash_sym, $rclass_name );
         }
     }
-    my $first = $name eq 'method' ? $op->first->save : $op->meth_sv->save;
+    my $first = $name eq 'method' ? $op->first->save("methop first") : $op->meth_sv->save("methop meth_sv");
 
     methopsect()->comment_common("first, rclass");
-    methopsect()->sadd( "%s, $union, (SV*)%s", $op->_save_common, $first, $rclass );
-    methopsect()->debug( $name, $flagspv ) if debug('flags');
-    my $sym = savesym( $op, "(OP*)&methop_list[$ix]" );    # save it before do_labels
+    methopsect()->supdate( $ix, "%s, $union, (SV*)%s", $op->_save_common, $first, $rclass );
+
+    #methopsect()->debug( $name, $flagspv ) if debug('flags');
     if ( $name eq 'method' ) {
         do_labels( $op, $level + 1, 'first', 'rclass' );
     }
