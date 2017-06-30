56f6e72d679200abe31bd4702d366774c9f58905
Todd Rinaldo <toddr@cpan.org>
2017-06-30 16:23:03 -0500

    RV.pm cleanup. should be a no-op

--- lib/B/C/OverLoad/B/RV.pm
+++ lib/B/C/OverLoad/B/RV.pm
@@ -3,7 +3,7 @@ package B::RV;
 use strict;
 
 use B::C::Config;
-use B::C::File qw/svsect init init2 init_static_assignments init_bootstraplink/;
+use B::C::File qw/svsect init init2 init_static_assignments /;
 use B::C::Helpers qw/is_constant/;
 
 use B::C::Helpers::Symtable qw/objsym savesym/;
@@ -11,34 +11,34 @@ use B::C::Helpers::Symtable qw/objsym savesym/;
 # Since 5.11 also called by IV::save (SV -> IV)
 sub do_save {
     my ( $sv, $fullname ) = @_;
+    $fullname ||= "(Unknown RV)";
 
-    debug(
-        sv => "Saving RV %s (0x%x) - called from %s:%s\n",
-        ref($sv), $$sv, @{ [ ( caller(1) )[3] ] }, @{ [ ( caller(1) )[2] ] }
-    );
+    debug( sv => "Saving RV %s (0x%x) - called from %s:%s\n", ref($sv), $$sv, @{ [ ( caller(1) )[3] ] }, @{ [ ( caller(1) )[2] ] } );
 
-    my $rv = save_op( $sv, $fullname );
-    return '0' if !$rv || $rv eq 'NULL' || $rv =~ qr{NULL$};
+    my $rv = $sv->RV->save($fullname);
+    return '0' if !$rv || $rv =~ m/NULL$/;
 
-    svsect()->comment("any, refcnt, flags, sv_u");
+    #svsect()->comment("any, refcnt, flags, sv_u");
+    my ( $ix, $sym ) = svsect()->reserve($sv);
+    svsect()->debug( $fullname, $sv );
 
     # 5.22 has a wrong RV->FLAGS (https://github.com/perl11/cperl/issues/63)
     my $flags = $sv->FLAGS;
     $flags = 0x801 if $flags & 9;    # not a GV but a ROK IV (21)
 
-    # 5.10 has no struct xrv anymore, just sv_u.svu_rv. static or dynamic?
-    # initializer element is computable at load time
-    my $ix = svsect()->sadd(
-        "NULL, %Lu, 0x%x, {%s}", $sv->REFCNT + 1, $flags,    # the SvANY is set just below at init time
-        ( is_constant($rv) ? ".svu_rv=$rv" : "0 /* $rv */" )
+    svsect()->supdatel(
+        $ix,
+        '(void*)%s - sizeof(void*)' => $sym,              # the SvANY is set just below at init time
+        '%Lu'                       => $sv->REFCNT + 1,
+        '0x%x'                      => $flags,
+        '{%s}', ( is_constant($rv) ? ".svu_rv=$rv" : "0 /* $rv */" )
     );
 
-    svsect()->debug( $fullname, $sv );
-    my $s = "sv_list[" . $ix . "]";
+    my $update_sym = $sym;
+    $update_sym =~ s/^&//;
 
-    init_static_assignments()->sadd( "%s.sv_any = (void*)&%s - sizeof(void*);", $s, $s );    # 354 defined needs SvANY
     if ( !is_constant($rv) ) {
-        my $init = $rv =~ /get_cv/ ? init2() : init();
+        my $init = ( $rv =~ /get_cv/ ) ? init2() : init();
 
         # check if the CV is bootsrrapped then use the correct section for it
         if ( my $sub = B::C::is_bootstrapped_cv($rv) ) {
@@ -46,10 +46,10 @@ sub do_save {
         }
 
         # ref($rv) ne 'B::GV' && ref($rv) ne 'B::HV'
-        $init->sadd( "%s.sv_u.svu_rv = (SV*)%s;", $s, $rv );
+        $init->sadd( "%s.sv_u.svu_rv = (SV*)%s;", $update_sym, $rv );
     }
 
-    return "&" . $s;
+    return $sym;
 }
 
 # the save methods should probably be renamed visit
@@ -59,7 +59,8 @@ sub save_op {    # previously known as 'sub save_rv'
     $fullname ||= '(unknown)';
 
     my $rv = $sv->RV->save($fullname);
-    $rv =~ s/^\(([AGHS]V|IO)\s*\*\)\s*(\&sv_list.*)$/$2/;
+
+    #$rv =~ s/^\(([AGHS]V|IO)\s*\*\)\s*(\&sv_list.*)$/$2/;
 
     return $rv;
 }
