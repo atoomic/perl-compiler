70414df788ab2809a0e1513f06ad87aa7e41c0bf
Todd Rinaldo <toddr@cpan.org>
2017-07-05 12:22:11 -0500

    use reserve for PVUV

--- lib/B/C/OverLoad/B/UV.pm
+++ lib/B/C/OverLoad/B/UV.pm
@@ -4,12 +4,14 @@ use strict;
 
 use B::C::Flags ();
 
-use B::C::Config;
-use B::C::File qw/svsect init/;
+use B::C::File qw/svsect/;
 use B::C::Decimal qw/u32fmt/;
 
 sub do_save {
     my ( $sv, $fullname ) = @_;
+
+    my ( $ix, $sym ) = svsect()->reserve($sv);
+    svsect()->debug( $fullname, $sv );
 
     my $uvuformat = $B::C::Flags::Config{uvuformat};
     $uvuformat =~ s/"//g;    #" poor editor
@@ -17,20 +19,16 @@ sub do_save {
     my $uvx = $sv->UVX;
     my $suff = $uvx > 2147483647 ? 'UL' : 'U';
 
-    my $sv_ix = svsect()->index + 1;
-    my $sym = sprintf( "&sv_list[%d]", $sv_ix );
-
     # Since 5.24 we can access the IV/NV/UV value from either the union from the main SV body
     # or also from the SvANY of it. View IV.pm for more information
 
-    svsect()->saddl(
+    svsect()->supdatel(
+        $ix,
         "BODYLESS_UV_PTR(%s)" => $sym,               # sv_any
         u32fmt()              => $sv->REFCNT + 1,    # sv_refcnt
         '0x%x'                => $sv->FLAGS,         # sv_flags
         '{.svu_uv=%s}'        => "$uvx$suff",        # sv_u.svu_uv
     );
-
-    svsect()->debug( $fullname, $sv );
 
     return $sym;
 }
