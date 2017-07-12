fb8c5c204749fe4a00f463b9b8d6998351b43c8d
Todd Rinaldo <toddr@cpan.org>
2017-07-12 00:23:57 -0500

    Audit PVLV.pm for STATIC_HV. ATM we think it's dead code anyway

--- lib/B/C/OverLoad/B/PVLV.pm
+++ lib/B/C/OverLoad/B/PVLV.pm
@@ -20,24 +20,7 @@ sub do_save {
     svsect()->debug( $fullname, $sv );
 
     my ( $pvsym, $cur, $len, $pv, $static, $flags ) = $sv->save_svu( $sym, $fullname );
-    my ( $lvtarg, $lvtarg_sym );    # XXX missing
 
-    #struct xpvlv {
-    #    _XPV_HEAD;
-    #    union _xivu xiv_u;
-    #    union _xnvu xnv_u;
-    #    union {
-    #        STRLEN  xlvu_targoff;
-    #        SSize_t xlvu_stargoff;
-    #    } xlv_targoff_u;
-    #    STRLEN      xlv_targlen;
-    #    SV*         xlv_targ;
-    #    char        xlv_type;       /* k=keys .=pos x=substr v=vec /=join/re
-    #                                 * y=alem/helem/iter t=tie T=tied HE */
-    #    char        xlv_flags;      /* 1 = negative offset  2 = negative len
-    #                                   4 = out of range (vec) */
-    #};
-    # STATIC HV: Static stash please.
     xpvlvsect()->comment('xmg_stash, xmg_u, xpv_cur, xpv_len_u, xiv_u, xnv_u, xlv_targoff_u, xlv_targlen, xlv_targ, xlv_type, xlv_flags');
     my $xpv_ix = xpvlvsect()->saddl(
         "%s"   => $sv->save_magic_stash,           # xmg_stash
