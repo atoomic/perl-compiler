32557134864f6e1cf349d4ea9e7e6118aa13244c
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-07-17 13:41:32 -0600

    xtestc/0173.t was a false positive
    The expected output appear at compile time
    as expected. We could improve our prove to
    check STDERR from compile output with some
    extra rules, but for now keep it simple.

--- t/testc.sh
+++ t/testc.sh
@@ -682,9 +682,10 @@ tests[173]='# WontFix
 use constant BEGIN   => 42; print "ok 1\n" if BEGIN == 42;
 use constant INIT   => 42; print "ok 2\n" if INIT == 42;
 use constant CHECK   => 42; print "ok 3\n" if CHECK == 42;'
-result[173]='Prototype mismatch: sub main::BEGIN () vs none at ./ccode173.pl line 2.
-Constant subroutine BEGIN redefined at ./ccode173.pl line 2.
-ok 1
+# these errors are at compile time as expected - need to check STDERR when compiling
+#stderr[173]='Prototype mismatch: sub main::BEGIN () vs none at ./ccode173.pl line 2.
+#Constant subroutine BEGIN redefined at ./ccode173.pl line 2.'
+result[173]='ok 1
 ok 2
 ok 3'
 tests[175]='{
