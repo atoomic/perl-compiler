ec146e268c966d192d4925c4a473065fb062222c
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-07-19 10:57:30 -0600

    Move some extra RegExp to init2

--- lib/B/C/OverLoad/B/REGEXP.pm
+++ lib/B/C/OverLoad/B/REGEXP.pm
@@ -37,7 +37,7 @@ sub do_save {
 
     my $initpm = init1();
 
-    if ( $pv =~ m/\\[pN]\{/ or $pv =~ m/\\U/ ) {
+    if ( $pv =~ m/\\[pN]\{/ or $cstr =~ m/\\[Ut]/ ) {
         $initpm = init2();
     }
 
