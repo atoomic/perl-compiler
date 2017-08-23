f1deec3411a03f22d969c3d4512882d4af7dcb21
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-23 12:39:51 -0600

    autosetup cpanel prove symlink

--- bamboo.sh
+++ bamboo.sh
@@ -3,3 +3,6 @@
 [ -e /usr/local/cpanel/3rdparty/perl/524/bin/perl ] && rpm -e --nodeps cpanel-perl-524 ||:
 rpm -Uv --force rpms/cpanel-perl-526-5.26.0-1.debuginfo.cp1170.x86_64.rpm
 
+echo "Setting prove for 526 - can also adjust symlinks"
+[ -L /usr/local/cpanel/3rdparty/bin/prove ] && rm -f /usr/local/cpanel/3rdparty/bin/prove
+ln -s /usr/local/cpanel/3rdparty/perl/526/bin/prove /usr/local/cpanel/3rdparty/bin/prove
