eaf6cf9d8210b799e45d304bb22bc2269c3f89be
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-23 12:04:08 -0600

    update-links.sh

--- t/testsuite/C-COMPILED/update-links.sh
+++ t/testsuite/C-COMPILED/update-links.sh
@@ -7,10 +7,11 @@ for d in "base" "cmd" "comp" "extra" "io" "mro" "op" "opbasic" "re" "uni"; do
     echo "* $d"
     # cleanup before link recreation
     rm -f *.t ||:
-    test -d ../../t/$d 
+    test -d ../../t/$d
     for t in ../../t/$d/*.t; do
         BN=$(basename $t)
         echo - updating: $BN
+        [ -f $BN ] && rm -f $BN ||:
         ln -s ../template.pl $BN
     done
     # remove threaded tests
