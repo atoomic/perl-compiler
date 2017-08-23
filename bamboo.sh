cf18e784288b3bf70aa78c1f7866616482821d12
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-23 12:43:38 -0600

    add comment

--- bamboo.sh
+++ bamboo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Removing perl524 & installing perl526"
 [ -e /usr/local/cpanel/3rdparty/perl/524/bin/perl ] && rpm -e --nodeps cpanel-perl-524 ||:
 rpm -Uv --force rpms/cpanel-perl-526-5.26.0-1.debuginfo.cp1170.x86_64.rpm
 
