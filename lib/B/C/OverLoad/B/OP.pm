67fd3f481ecd27694b1fcb1fdccf1a9a57fd5efb
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-28 10:11:53 -0600

    define BASEOP without PERL_OP_PARENT
    preserve hardcoded values for now

--- lib/B/C/OverLoad/B/OP.pm
+++ lib/B/C/OverLoad/B/OP.pm
@@ -60,12 +60,37 @@ sub B::OP::fake_ppaddr {
 sub _save_common {
     my $op = shift;
 
-    return sprintf(
-        "%s, %s, %s, %u, %u, 0, 0, 0, 1, 0, 0, 0, 0x%x, 0x%x",
-        $op->next->save    || 'NULL',
-        $op->sibling->save || 'NULL',
-        $op->fake_ppaddr, $op->targ, $op->type, $op->flags || 0, $op->private || 0
+    # view BASEOP in op.h
+    # increase readability by using an array
+    my @BASEOP = (
+        '%s' => $op->next->save    || 'NULL', # OP*     op_next;
+        '%s' => $op->sibling->save || 'NULL',  # OP*     op_sibparent;\ # instead of op_sibling
+        '%s' => $op->fake_ppaddr,             # OP*     (*op_ppaddr)(pTHX);
+        '%u' => $op->targ,                    # PADOFFSET   op_targ;
+        '%u' => $op->type,                    # PERL_BITFIELD16 op_type:9;
+        '%u' => 0, # $op->opt || 0,               # PERL_BITFIELD16 op_opt:1; -- was hardcoded to 0
+        '%u' => 0, # $op->slabbed || 0,            # PERL_BITFIELD16 op_slabbed:1; -- was hardcoded to 0
+        '%u' => 0, # $op->savefree || 0,           # PERL_BITFIELD16 op_savefree:1; -- was hardcoded to 0
+        '%u' => 1,                            # PERL_BITFIELD16 op_static:1; -- is hardcoded to 1
+        '%u' => 0, # $op->folded || 0,             # PERL_BITFIELD16 op_folded:1; -- was hardcoded to 0
+        '%u' => 0, # $op->moresib || 0,            # PERL_BITFIELD16 op_moresib:1; -- was hardcoded to 0
+        '%u' => 0, # $op->spare || 0,              # PERL_BITFIELD16 op_spare:1; -- was hardcoded to 0
+        '0x%x' => $op->flags || 0,            # U8      op_flags;
+        '0x%x' => $op->private || 0           # U8      op_private;
     );
+
+    die qq[BASEOP definition need an even number of args] if scalar @BASEOP % 2; # sanity check
+
+    # some syntactic sugar
+    my ( @keys, @values );
+
+    while ( scalar @BASEOP ) {
+        push @keys, shift @BASEOP; # key
+        push @values, shift @BASEOP; # value
+    }
+
+    my $template = join ', ', @keys;
+    return sprintf( $template, @values );
 }
 
 # XXX HACK! duct-taping around compiler problems
