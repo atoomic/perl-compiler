4037494c5d9a3018d257a5af8d0df177711dd333
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-30 15:15:33 -0600

    Reduce deep recursion with BINOP
    (cherry picked from commit e58deae27cf296a814d096dc8e82afd23f4af3a5)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>

--- lib/B/C/OverLoad/B/BINOP.pm
+++ lib/B/C/OverLoad/B/BINOP.pm
@@ -16,6 +16,14 @@ sub do_save {
     my ( $ix, $sym ) = binopsect()->reserve( $op, "OP*" );
     binopsect->debug( $op->name, $op->flagspv );
 
+    # view Sub::Call::Tail perldoc for more details ( could use it )
+    local @_ = ( $ix, $sym, $op );
+    goto &do_update;    # avoid deep recursion calls by forcing a tail call with goto
+}
+
+sub do_update {
+    my ( $ix, $sym, $op ) = @_;
+
     binopsect->supdate( $ix, "%s, %s, %s", $op->save_baseop, $op->first->save, $op->last->save );
 
     my $ppaddr = $op->ppaddr;
