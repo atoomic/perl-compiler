729e9be40115176ea9e43e7c8b498cc85c26e227
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-30 15:45:23 -0600

    Reduce deep recursion with LISTOP
    (cherry picked from commit 4d94766a5d0068105dff094bf3692ad6c2bd599d)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>

--- lib/B/C/OverLoad/B/LISTOP.pm
+++ lib/B/C/OverLoad/B/LISTOP.pm
@@ -11,6 +11,14 @@ sub do_save {
     my ( $ix, $sym ) = listopsect()->reserve( $op, "OP*" );
     listopsect()->debug( $op->name, $op );
 
+    # view Sub::Call::Tail perldoc for more details ( could use it )
+    local @_ = ( $ix, $sym, $op );
+    goto &do_update;    # avoid deep recursion calls by forcing a tail call with goto
+}
+
+sub do_update {
+    my ( $ix, $sym, $op ) = @_;
+
     listopsect()->supdate( $ix, "%s, %s, %s", $op->save_baseop, $op->first->save, $op->last->save );
 
     return $sym;
