511823aed422cdc55cddd2e45c556eacb2837451
J. Nick Koston <nick@cpanel.net>
2017-09-06 22:03:50 -0500

    Save an empty string for $1,$2,....
    Apply some minor fixup
    - flag the $match as a COW string
    - make sure $10, $20, $30... are treated the same way
    as other match capture
    
    (cherry picked from commit 8db26b98e564378780e492f511fa6e665ac1cbd9)
    Signed-off-by: Nicolas Rochelemagne <rochelemagne@cpanel.net>

--- lib/B/C/OverLoad/B/PVMG.pm
+++ lib/B/C/OverLoad/B/PVMG.pm
@@ -3,7 +3,7 @@ package B::PVMG;
 use strict;
 
 use B::C::Debug qw/debug verbose WARN/;
-use B qw/SVf_READONLY SVf_READONLY cchar SVp_POK svref_2object/;
+use B qw/SVf_IsCOW SVf_READONLY cchar SVp_POK svref_2object/;
 use B::C::Save qw/savecowpv/;
 use B::C::Decimal qw/get_integer_value get_double_value/;
 use B::C::File qw/init init_static_assignments svsect xpvmgsect magicsect init_vtables/;
@@ -19,10 +19,20 @@ sub do_save {
     my ( $ix, $sym ) = svsect()->reserve($sv);
     svsect()->debug( $fullname, $sv );
 
-    my ( $sv_u, $cur, $len, $pv, $flags ) = $sv->save_svu( $sym, $sym, $fullname );
+    my ( $sv_u, $cur, $len, $pv, $flags );
+    if ( $fullname =~ m{^main::[1-9][0-9]*$} ) {    # Only modify $1,$2, ... etc.  $0 is magic
+        $flags = $sv->FLAGS;
+        $flags |= SVf_IsCOW;                        # flag it as COW as we are using the generic empty string
+        $pv = "";
+        ( $sv_u, $cur, $len ) = savecowpv($pv);
+        $sv_u = ".svu_pv=(char*) $sv_u";
+    }
+    else {
+        ( $sv_u, $cur, $len, $pv, $flags ) = $sv->save_svu( $sym, $sym, $fullname );
+    }
 
-    my $ivx = get_integer_value( $sv->IVX );    # XXX How to detect HEK* namehek?
-    my $nvx = get_double_value( $sv->NVX );     # it cannot be xnv_u.xgv_stash ptr (BTW set by GvSTASH later)
+    my $ivx = get_integer_value( $sv->IVX );        # XXX How to detect HEK* namehek?
+    my $nvx = get_double_value( $sv->NVX );         # it cannot be xnv_u.xgv_stash ptr (BTW set by GvSTASH later)
 
     # See #305 Encode::XS: XS objects are often stored as SvIV(SvRV(obj)). The real
     # address needs to be patched after the XS object is initialized.
