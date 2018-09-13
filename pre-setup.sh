514a57170f1e5eb865cfc6deea678d6de3cdf876
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2018-09-13 13:17:20 -0500

    install custom RPMS: DBI, DBD::Pg and DBD::SQLite
    we now have them in place, try to install them
    to check if this fixes the DBI tests.

--- pre-setup.sh
+++ pre-setup.sh
@@ -15,6 +15,14 @@ cd src
 
 /usr/local/cpanel/3rdparty/bin/perl528 -MTAP::Formatter::JUnit -E "say q[perl installed with TAP::Formatter::JUnit];"
 
+# we now whave some custom RPMs available install and use them if possible
+SRC="https://vmware-manager.dev.cpanel.net/RPM/11.78/centos/7/x86_64"
+rpm -Uv \
+    "$SRC/cpanel-perl-528-DBI-1.641-1.cp1178.x86_64.rpm" \
+    "$SRC/cpanel-perl-528-DBD-SQLite-1.58-1.cp1178.x86_64.rpm" \
+    "$SRC/cpanel-perl-528-DBD-Pg-3.7.4-1.cp1178.x86_64.rpm"
+
 #/usr/local/cpanel/3rdparty/bin/perl528 -MTAP::Formatter::JUnit -E "say q[can find TAP::Formatter::JUnit]; use Test::More; note explain \%INC"
 
+echo "Using prove: "
 ls -l /usr/local/cpanel/3rdparty/bin/prove
