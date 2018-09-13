8d7f1cd0dbd539a35a0a8e547685b4292ad9a0b1
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2018-09-13 13:04:11 -0500

    Use the renamed perl-528 RPMs using cp1178

--- pre-setup.sh
+++ pre-setup.sh
@@ -3,7 +3,8 @@
 echo "Removing perl524 / perl526 & installing perl528"
 [ -e /usr/local/cpanel/3rdparty/perl/524/bin/perl ] && rpm -e --nodeps cpanel-perl-524 ||:
 [ -e /usr/local/cpanel/3rdparty/perl/526/bin/perl ] && rpm -e --nodeps cpanel-perl-526 ||:
-rpm -Uv --force rpms/cpanel-perl-528-5.28.0-0.cp1170.x86_64.rpm
+
+rpm -Uv --force rpms/cpanel-perl-528-5.28.0-0.cp1178.x86_64.rpm
 
 echo "Setting prove for 528 - can also adjust symlinks"
 [ -L /usr/local/cpanel/3rdparty/bin/prove ] && rm -f /usr/local/cpanel/3rdparty/bin/prove
