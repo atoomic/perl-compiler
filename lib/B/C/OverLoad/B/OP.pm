184a8d20907f817de1098156749e8ae5837ac2c8
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2019-05-22 17:27:41 -0500

    Replace type by constant when possible

--- lib/B/C/OverLoad/B/OP.pm
+++ lib/B/C/OverLoad/B/OP.pm
@@ -2,13 +2,16 @@ package B::OP;
 
 use strict;
 
-use B qw/opnumber/;
+use B qw/opnumber ppname/;
 
 use B::C::Debug qw/debug verbose/;
 use B::C::File qw/opsect init_xops/;
 use B::C::Helpers::Symtable qw/objsym/;
 
 my $OP_CUSTOM = opnumber('custom');
+
+# cannot use opnumber for OP_MAX
+# we also need C.xs to be loaded from B::C_heavy to use B::OP::max()
 
 # special handling for nullified COP's.
 my %OP_COP = ( opnumber('nextstate') => 1 );
@@ -77,27 +80,56 @@ sub basop_comment {
     return "next, sibparent, ppaddr, targ, type, opt, slabbed, savefree, static, folded, moresib, spare, flags, private";
 }
 
+my %_id2enum;
+
+sub optype_id2enum {
+    my ($id) = @_;
+
+    return unless defined $id;
+
+    if ( !exists $_id2enum{$id} ) {
+        $_id2enum{$id} = undef;    # preset it to undef
+        if ( my $ppname = ppname($id) ) {    # already checking we are in the range 0 <= B::OP::max()
+            my $opname = uc("$ppname");
+            $opname =~ s{^PP_}{OP_} or die "# ... Failed to replace pp_ for $ppname";
+
+            # { # sanity check
+            #     my $x = $_id2enum{$id};
+            #     $x =~ s{^OP_}{};
+            #     my $xid = opnumber( lc $x );
+            #     print STDERR "--- $ppname : $id vs $xid\n";
+            #     die "OP2ENUM sanity check failed $ppname : $id vs $xid" unless $id eq $xid;
+            # }
+
+            # only set it when we know can use it
+            $_id2enum{$id} = $opname;
+        }
+    }
+
+    return $_id2enum{$id};
+}
+
 sub save_baseop {
     my $op = shift;
 
     # view BASEOP in op.h
     # increase readability by using an array
     my @BASEOP = (
-        '%s'   => $op->next->save,         # OP*     op_next;
-                                           # this is using 'sibparent' method from our custom C.xs function - consider submit it upstream
-        '%s'   => $op->sibparent->save,    # OP*     op_sibparent; # could be previously op_sibling
-        '%s'   => $op->fake_ppaddr,        # OP*     (*op_ppaddr)(pTHX);
-        '%u'   => $op->targ,               # PADOFFSET   op_targ;
-        '%u'   => $op->type,               # PERL_BITFIELD16 op_type:9;
-        '%u'   => $op->opt || 0,           # PERL_BITFIELD16 op_opt:1; -- was hardcoded to 0
-        '%u'   => 0,                       # $op->slabbed || 0,            # PERL_BITFIELD16 op_slabbed:1; -- was hardcoded to 0
-        '%u'   => $op->savefree || 0,      # PERL_BITFIELD16 op_savefree:1; -- was hardcoded to 0
-        '%u'   => 1,                       # PERL_BITFIELD16 op_static:1; -- is hardcoded to 1
-        '%u'   => $op->folded || 0,        # PERL_BITFIELD16 op_folded:1; -- was hardcoded to 0
-        '%u'   => $op->moresib || 0,       # PERL_BITFIELD16 op_moresib:1; -- was hardcoded to 0
-        '%u'   => $op->spare || 0,         # PERL_BITFIELD16 op_spare:1; -- was hardcoded to 0
-        '0x%x' => $op->flags || 0,         # U8      op_flags;
-        '0x%x' => $op->private || 0        # U8      op_private;
+        '%s'   => $op->next->save,                # OP*     op_next;
+                                                  # this is using 'sibparent' method from our custom C.xs function - consider submit it upstream
+        '%s'   => $op->sibparent->save,           # OP*     op_sibparent; # could be previously op_sibling
+        '%s'   => $op->fake_ppaddr,               # OP*     (*op_ppaddr)(pTHX);
+        '%u'   => $op->targ,                      # PADOFFSET   op_targ;
+        '%s'   => optype_id2enum( $op->type ),    # PERL_BITFIELD16 op_type:9;
+        '%u'   => $op->opt || 0,                  # PERL_BITFIELD16 op_opt:1; -- was hardcoded to 0
+        '%u'   => 0,                              # $op->slabbed || 0,            # PERL_BITFIELD16 op_slabbed:1; -- was hardcoded to 0
+        '%u'   => $op->savefree || 0,             # PERL_BITFIELD16 op_savefree:1; -- was hardcoded to 0
+        '%u'   => 1,                              # PERL_BITFIELD16 op_static:1; -- is hardcoded to 1
+        '%u'   => $op->folded || 0,               # PERL_BITFIELD16 op_folded:1; -- was hardcoded to 0
+        '%u'   => $op->moresib || 0,              # PERL_BITFIELD16 op_moresib:1; -- was hardcoded to 0
+        '%u'   => $op->spare || 0,                # PERL_BITFIELD16 op_spare:1; -- was hardcoded to 0
+        '0x%x' => $op->flags || 0,                # U8      op_flags;
+        '0x%x' => $op->private || 0               # U8      op_private;
     );
 
     die qq[BASEOP definition need an even number of args] if scalar @BASEOP % 2;    # sanity check
